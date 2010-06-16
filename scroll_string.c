44c0ad63f5cead5ae81441aca192462ca53b6a13
Vince Weaver <vince@atom.deaternet>
2010-06-16 12:46:00 -0400

    16 June 2010 + Add scroll_string demo

---
+++ scroll_string.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "meter.h"
+#include "meter_tools.h"
+
+int main(int argc, char **argv) {
+
+  int i,len=0,pos=0;
+  unsigned short display_state[8];
+  char string[256];
+
+  init_meter();
+   
+  reset_display();
+  display_config_sane();
+  for(i=0;i<8;i++) { display_state[i]=0; }
+  update_display(display_state);   
+  
+  while(1) {
+    if (fgets(string,256,stdin)==NULL) break;
+    
+        len=strlen(string);
+        pos=0;
+    
+        while(1) {
+    
+
+           for(i=0;i<6;i++) {
+           if (pos+i<len-1) {
+	      display_state[i]=ascii_lookup[(unsigned char)string[pos+i]];
+           }
+           else display_state[i]=0;
+       }
+
+
+       update_display(display_state);   
+       /* pause longer if first char */
+       if (pos==0) usleep(500000);
+       usleep(500000);
+
+       pos++;
+       if (pos==len) break;
+    }
+
+    
+  }
+
+  return 0;
+}
+
+
