4be523a0f92444c1c9f0da01728b7cc5d73acca2
Vince Weaver <vince@atom.deaternet>
2011-02-21 13:30:00 -0500

    Some changes to the hardware

---
+++ hardware/led_board/vmw_meter_led_board.cmd
@@ -0,0 +1,262 @@
+# Pin name action command file
+
+# Start of element D7
+ChangePinName(D7, 2, 2)
+ChangePinName(D7, 1, 1)
+
+# Start of element D8
+ChangePinName(D8, 2, 2)
+ChangePinName(D8, 1, 1)
+
+# Start of element D9
+ChangePinName(D9, 2, 2)
+ChangePinName(D9, 1, 1)
+
+# Start of element D10
+ChangePinName(D10, 2, 2)
+ChangePinName(D10, 1, 1)
+
+# Start of element D11
+ChangePinName(D11, 2, 2)
+ChangePinName(D11, 1, 1)
+
+# Start of element D12
+ChangePinName(D12, 2, 2)
+ChangePinName(D12, 1, 1)
+
+# Start of element D13
+ChangePinName(D13, 2, 2)
+ChangePinName(D13, 1, 1)
+
+# Start of element D14
+ChangePinName(D14, 2, 2)
+ChangePinName(D14, 1, 1)
+
+# Start of element D15
+ChangePinName(D15, 2, 2)
+ChangePinName(D15, 1, 1)
+
+# Start of element D16
+ChangePinName(D16, 2, 2)
+ChangePinName(D16, 1, 1)
+
+# Start of element D17
+ChangePinName(D17, 2, 2)
+ChangePinName(D17, 1, 1)
+
+# Start of element D18
+ChangePinName(D18, 2, 2)
+ChangePinName(D18, 1, 1)
+
+# Start of element SOCKET4
+ChangePinName(SOCKET4, 20, A)
+ChangePinName(SOCKET4, 19, B)
+ChangePinName(SOCKET4, 18, C)
+ChangePinName(SOCKET4, 17, D)
+ChangePinName(SOCKET4, 16, E)
+ChangePinName(SOCKET4, 15, F)
+ChangePinName(SOCKET4, 14, G)
+ChangePinName(SOCKET4, 13, H)
+ChangePinName(SOCKET4, 12, GND)
+ChangePinName(SOCKET4, 11, D1)
+ChangePinName(SOCKET4, 10, D2)
+ChangePinName(SOCKET4, 9, GND)
+ChangePinName(SOCKET4, 8, EX)
+ChangePinName(SOCKET4, 7, DP)
+ChangePinName(SOCKET4, 6, P)
+ChangePinName(SOCKET4, 5, N)
+ChangePinName(SOCKET4, 4, M)
+ChangePinName(SOCKET4, 3, L)
+ChangePinName(SOCKET4, 2, K)
+ChangePinName(SOCKET4, 1, J)
+
+# Start of element LED5
+ChangePinName(LED5, 20, CA)
+ChangePinName(LED5, 19, CB)
+ChangePinName(LED5, 18, CC)
+ChangePinName(LED5, 17, CD)
+ChangePinName(LED5, 16, CE)
+ChangePinName(LED5, 15, CF)
+ChangePinName(LED5, 14, CG)
+ChangePinName(LED5, 13, CH)
+ChangePinName(LED5, 12, CJ)
+ChangePinName(LED5, 11, CK)
+ChangePinName(LED5, 10, AK)
+ChangePinName(LED5, 9, AJ)
+ChangePinName(LED5, 8, AH)
+ChangePinName(LED5, 7, AG)
+ChangePinName(LED5, 6, AF)
+ChangePinName(LED5, 5, AE)
+ChangePinName(LED5, 4, AD)
+ChangePinName(LED5, 3, AC)
+ChangePinName(LED5, 2, AB)
+ChangePinName(LED5, 1, AA)
+
+# Start of element LED4
+ChangePinName(LED4, 20, CA)
+ChangePinName(LED4, 19, CB)
+ChangePinName(LED4, 18, CC)
+ChangePinName(LED4, 17, CD)
+ChangePinName(LED4, 16, CE)
+ChangePinName(LED4, 15, CF)
+ChangePinName(LED4, 14, CG)
+ChangePinName(LED4, 13, CH)
+ChangePinName(LED4, 12, CJ)
+ChangePinName(LED4, 11, CK)
+ChangePinName(LED4, 10, AK)
+ChangePinName(LED4, 9, AJ)
+ChangePinName(LED4, 8, AH)
+ChangePinName(LED4, 7, AG)
+ChangePinName(LED4, 6, AF)
+ChangePinName(LED4, 5, AE)
+ChangePinName(LED4, 4, AD)
+ChangePinName(LED4, 3, AC)
+ChangePinName(LED4, 2, AB)
+ChangePinName(LED4, 1, AA)
+
+# Start of element D6
+ChangePinName(D6, 2, 2)
+ChangePinName(D6, 1, 1)
+
+# Start of element D5
+ChangePinName(D5, 2, 2)
+ChangePinName(D5, 1, 1)
+
+# Start of element LED3
+ChangePinName(LED3, 18, F)
+ChangePinName(LED3, 17, P)
+ChangePinName(LED3, 16, D1)
+ChangePinName(LED3, 15, G)
+ChangePinName(LED3, 14, H)
+ChangePinName(LED3, 13, N)
+ChangePinName(LED3, 12, A)
+ChangePinName(LED3, 11, D2)
+ChangePinName(LED3, 10, B)
+ChangePinName(LED3, 9, C)
+ChangePinName(LED3, 8, DP)
+ChangePinName(LED3, 7, D)
+ChangePinName(LED3, 6, J)
+ChangePinName(LED3, 5, K)
+ChangePinName(LED3, 4, L)
+ChangePinName(LED3, 2, M)
+ChangePinName(LED3, 1, E)
+
+# Start of element SOCKET3
+ChangePinName(SOCKET3, 20, A)
+ChangePinName(SOCKET3, 19, B)
+ChangePinName(SOCKET3, 18, C)
+ChangePinName(SOCKET3, 17, D)
+ChangePinName(SOCKET3, 16, E)
+ChangePinName(SOCKET3, 15, F)
+ChangePinName(SOCKET3, 14, G)
+ChangePinName(SOCKET3, 13, H)
+ChangePinName(SOCKET3, 12, GND)
+ChangePinName(SOCKET3, 11, D1)
+ChangePinName(SOCKET3, 10, D2)
+ChangePinName(SOCKET3, 9, GND)
+ChangePinName(SOCKET3, 8, EX)
+ChangePinName(SOCKET3, 7, DP)
+ChangePinName(SOCKET3, 6, P)
+ChangePinName(SOCKET3, 5, N)
+ChangePinName(SOCKET3, 4, M)
+ChangePinName(SOCKET3, 3, L)
+ChangePinName(SOCKET3, 2, K)
+ChangePinName(SOCKET3, 1, J)
+
+# Start of element D4
+ChangePinName(D4, 2, 2)
+ChangePinName(D4, 1, 1)
+
+# Start of element D3
+ChangePinName(D3, 2, 2)
+ChangePinName(D3, 1, 1)
+
+# Start of element LED2
+ChangePinName(LED2, 18, F)
+ChangePinName(LED2, 17, P)
+ChangePinName(LED2, 16, D1)
+ChangePinName(LED2, 15, G)
+ChangePinName(LED2, 14, H)
+ChangePinName(LED2, 13, N)
+ChangePinName(LED2, 12, A)
+ChangePinName(LED2, 11, D2)
+ChangePinName(LED2, 10, B)
+ChangePinName(LED2, 9, C)
+ChangePinName(LED2, 8, DP)
+ChangePinName(LED2, 7, D)
+ChangePinName(LED2, 6, J)
+ChangePinName(LED2, 5, K)
+ChangePinName(LED2, 4, L)
+ChangePinName(LED2, 2, M)
+ChangePinName(LED2, 1, E)
+
+# Start of element SOCKET2
+ChangePinName(SOCKET2, 20, A)
+ChangePinName(SOCKET2, 19, B)
+ChangePinName(SOCKET2, 18, C)
+ChangePinName(SOCKET2, 17, D)
+ChangePinName(SOCKET2, 16, E)
+ChangePinName(SOCKET2, 15, F)
+ChangePinName(SOCKET2, 14, G)
+ChangePinName(SOCKET2, 13, H)
+ChangePinName(SOCKET2, 12, GND)
+ChangePinName(SOCKET2, 11, D1)
+ChangePinName(SOCKET2, 10, D2)
+ChangePinName(SOCKET2, 9, GND)
+ChangePinName(SOCKET2, 8, EX)
+ChangePinName(SOCKET2, 7, DP)
+ChangePinName(SOCKET2, 6, P)
+ChangePinName(SOCKET2, 5, N)
+ChangePinName(SOCKET2, 4, M)
+ChangePinName(SOCKET2, 3, L)
+ChangePinName(SOCKET2, 2, K)
+ChangePinName(SOCKET2, 1, J)
+
+# Start of element D2
+ChangePinName(D2, 2, 2)
+ChangePinName(D2, 1, 1)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element LED1
+ChangePinName(LED1, 18, F)
+ChangePinName(LED1, 17, P)
+ChangePinName(LED1, 16, D1)
+ChangePinName(LED1, 15, G)
+ChangePinName(LED1, 14, H)
+ChangePinName(LED1, 13, N)
+ChangePinName(LED1, 12, A)
+ChangePinName(LED1, 11, D2)
+ChangePinName(LED1, 10, B)
+ChangePinName(LED1, 9, C)
+ChangePinName(LED1, 8, DP)
+ChangePinName(LED1, 7, D)
+ChangePinName(LED1, 6, J)
+ChangePinName(LED1, 5, K)
+ChangePinName(LED1, 4, L)
+ChangePinName(LED1, 2, M)
+ChangePinName(LED1, 1, E)
+
+# Start of element SOCKET1
+ChangePinName(SOCKET1, 20, A)
+ChangePinName(SOCKET1, 19, B)
+ChangePinName(SOCKET1, 18, C)
+ChangePinName(SOCKET1, 17, D)
+ChangePinName(SOCKET1, 16, E)
+ChangePinName(SOCKET1, 15, F)
+ChangePinName(SOCKET1, 14, G)
+ChangePinName(SOCKET1, 13, H)
+ChangePinName(SOCKET1, 12, GND)
+ChangePinName(SOCKET1, 11, D1)
+ChangePinName(SOCKET1, 10, D2)
+ChangePinName(SOCKET1, 9, GND)
+ChangePinName(SOCKET1, 8, EX)
+ChangePinName(SOCKET1, 7, DP)
+ChangePinName(SOCKET1, 6, P)
+ChangePinName(SOCKET1, 5, N)
+ChangePinName(SOCKET1, 4, M)
+ChangePinName(SOCKET1, 3, L)
+ChangePinName(SOCKET1, 2, K)
+ChangePinName(SOCKET1, 1, J)
