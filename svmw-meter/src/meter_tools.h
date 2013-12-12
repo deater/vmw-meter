328902f6fca2f4da246a8db86db6ba92e35282e0
Vince Weaver <vincent.weaver@maine.edu>
2013-12-12 15:23:08 -0500

    svmw-meter: finish conversion to new interface

--- svmw-meter/src/meter_tools.h
+++
@@ -1,3 +0,0 @@
-void display_config_sane();
-void update_display(unsigned short state[8]);
-extern short ascii_lookup[256];
--- svmw-meter/src/set_display.c
+++ svmw-meter/src/set_display.c
@@ -5,64 +5,79 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#include "meter.h"
-#include "meter_tools.h"
+#include "14seg_font.h"
+#include "i2c_lib.h"
 
 int main(int argc, char **argv) {
 
-  int i;
-  unsigned short display_state[8];
-  char string[256],*endptr;
-  int top,dp,left,right;
-   
-  if (argc<6) {
-     printf("Usage: %s string top_leds decimal_points left_bar right_bar\n",
-	    argv[0]);
-     exit(1);
-  }
+	int i;
+	unsigned short display_state[8];
+	char string[256],*endptr;
+	int top,dp,left,right;
+	int meter_fd,display_present;
 
-  strcpy(string,argv[1]);
-   
-  init_meter();
-   
-  reset_display();
-  display_config_sane();
-   
-  for(i=0;i<8;i++) { display_state[i]=0; }
-  update_display(display_state);   
-  
-   /* set string */
-  for(i=0;i<6;i++) {
-     if (i<strlen(string)) {
-	display_state[i]=ascii_lookup[(unsigned char)string[i]];
-      }
-      else display_state[i]=0;
-  }
-   
-  /* top */
-  top=strtol(argv[2], &endptr, 0);
-  for(i=0;i<6;i++) {
-     if (top&(1<<(5-i))) display_state[i]|=SEGMENT_EX;
-  }
-   
-  /* dp */
-  dp=strtol(argv[3], &endptr, 0);
-  for(i=0;i<6;i++) {
-     if (dp&(1<<(5-i))) display_state[i]|=SEGMENT_DP;
-  }   
-   
-  /* left */
-  left=strtol(argv[4], &endptr, 0);
-  display_state[6]=left;
- 
-  /* right */
-  right=strtol(argv[5], &endptr, 0);
-  display_state[7]=right;
+	if (argc<6) {
+		printf("Usage: %s string top_leds decimal_points left_bar right_bar\n",
+			argv[0]);
+		exit(1);
+	}
 
-  /* update the display */
-  update_display(display_state);   
+	strcpy(string,argv[1]);
 
-  return 0;
+	display_present=1;
+	meter_fd=init_i2c("/dev/i2c-6");
+	if (meter_fd < 0) {
+		fprintf(stderr,"Error opening device!\n");
+		display_present=0;
+	}
+
+	if (display_present) {
+		init_saa1064_display(meter_fd,SAA1064_ADDRESS1);
+		init_saa1064_display(meter_fd,SAA1064_ADDRESS2);
+		init_saa1064_display(meter_fd,SAA1064_ADDRESS3);
+		init_saa1064_display(meter_fd,SAA1064_ADDRESS4);
+	}
+
+
+	/* set string */
+	for(i=0;i<6;i++) {
+		if (i<strlen(string)) {
+			display_state[i]=ascii_lookup[(unsigned char)string[i]];
+		}
+		else display_state[i]=0;
+	}
+
+	/* top */
+	top=strtol(argv[2], &endptr, 0);
+	for(i=0;i<6;i++) {
+		if (top&(1<<(5-i))) display_state[i]|=SAA1064_SEGMENT_EX;
+	}
+
+	/* dp */
+	dp=strtol(argv[3], &endptr, 0);
+	for(i=0;i<6;i++) {
+		if (dp&(1<<(5-i))) display_state[i]|=SAA1064_SEGMENT_DP;
+	}
+
+	/* left */
+	left=strtol(argv[4], &endptr, 0);
+	display_state[6]=left;
+
+	/* right */
+	right=strtol(argv[5], &endptr, 0);
+	display_state[7]=right;
+
+	/* update the display */
+
+	if (display_present) {
+		update_saa1064_display(meter_fd, SAA1064_ADDRESS1,
+			display_state);
+	}
+	else {
+		update_saa1064_ascii(display_state);
+	}
+
+  	return 0;
 }
 
 
