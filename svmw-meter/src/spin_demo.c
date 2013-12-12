a8a7cae88a73ca682b6742134574eca8cbb10f50
Vince Weaver <vincent.weaver@maine.edu>
2013-12-12 15:07:23 -0500

    svmw-meter: update spin_demo to new interface

--- svmw-meter/src/spin_demo.c
+++ svmw-meter/src/spin_demo.c
@@ -5,53 +5,69 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "meter.h"
-#include "meter_tools.h"
+#include "i2c_lib.h"
 
 int main(int argc, char **argv) {
 
-  int i,spin=0,dot1=0,dot2=19,blob=0;
-  unsigned short display_state[8];
+	int i,spin=0,dot1=0,dot2=19,blob=0;
+	unsigned short display_state[8];
+	int meter_fd,display_present;
 
-  init_meter();
-  reset_display();
-  display_config_sane();
-  
-  while(1) {
-     for(i=0;i<6;i++) {
-       display_state[i]=0xfc00|(0x0200>>spin);              
-       //       printf("%d %d %x %x\n",i,spin,0x2000,display_state[i]);
-     }
-     spin++;
-     if (spin>7) spin=0;
+	display_present=1;
+	meter_fd=init_i2c("/dev/i2c-6");
+	if (meter_fd < 0) {
+		fprintf(stderr,"Error opening device!\n");
+		display_present=0;
+	}
 
-     display_state[6]=0;
-     display_state[7]=0;
+	if (display_present) {
+		init_saa1064_display(meter_fd,SAA1064_ADDRESS1);
+		init_saa1064_display(meter_fd,SAA1064_ADDRESS2);
+		init_saa1064_display(meter_fd,SAA1064_ADDRESS3);
+		init_saa1064_display(meter_fd,SAA1064_ADDRESS4);
+	}
 
-     if (dot1<16) display_state[6]|=1<<dot1;
-     else if (dot1<22) display_state[dot1-16]|=SEGMENT_DP;
-     else display_state[7]=1<<(15-(dot1-22));
+	while(1) {
+		for(i=0;i<6;i++) {
+			display_state[i]=0xfc00|(0x0200>>spin);
+		}
+		spin++;
+		if (spin>7) spin=0;
 
-     if (dot2<16) display_state[6]|=1<<dot2;
-     else if (dot2<22) display_state[dot2-16]|=SEGMENT_DP;
-     else display_state[7]=1<<(15-(dot2-22));
+		display_state[6]=0;
+		display_state[7]=0;
 
-     dot1++;
-     dot2++;
-     if (dot1>37) dot1=0;
-     if (dot2>37) dot2=0;
+		if (dot1<16) display_state[6]|=1<<dot1;
+		else if (dot1<22) display_state[dot1-16]|=SAA1064_SEGMENT_DP;
+		else display_state[7]=1<<(15-(dot1-22));
 
-     if (spin%2) blob=rand();
-     for(i=0;i<6;i++) {
-       if (blob&1<<i) display_state[i]|=SEGMENT_EX;
-     }
- 
-     update_display(display_state);   
-     usleep(100000);
-  }
+		if (dot2<16) display_state[6]|=1<<dot2;
+		else if (dot2<22) display_state[dot2-16]|=SAA1064_SEGMENT_DP;
+		else display_state[7]=1<<(15-(dot2-22));
+
+		dot1++;
+		dot2++;
+		if (dot1>37) dot1=0;
+		if (dot2>37) dot2=0;
+
+		if (spin%2) blob=rand();
+		for(i=0;i<6;i++) {
+			if (blob&1<<i) display_state[i]|=SAA1064_SEGMENT_EX;
+		}
+
+		if (display_present) {
+			update_saa1064_display(meter_fd, SAA1064_ADDRESS1,
+				display_state);
+		}
+		else {
+			update_saa1064_ascii(display_state);
+		}
+
+		usleep(100000);
+	}
 
 
-  return 0;
+	return 0;
 }
 
 
