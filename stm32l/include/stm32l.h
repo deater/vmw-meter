4b293c267aadcc1337589ea86287e0734102b0a8
Vince Weaver <vincent.weaver@maine.edu>
2014-01-24 17:14:14 -0500

    stm32l: lcd code
    it doesn't work, mysterious, SR UDR bit never goes low

--- stm32l/include/stm32l.h
+++ stm32l/include/stm32l.h
@@ -1,7 +1,112 @@
 #define __IO volatile
 
 #define PERIPH_BASE	((uint32_t)0x40000000)
+#define APB1PERIPH_BASE	PERIPH_BASE
 #define AHBPERIPH_BASE	(PERIPH_BASE + 0x20000)
+
+/* LCD */
+typedef struct {
+__IO uint32_t CR;	/* LCD control register		0x00 */
+__IO uint32_t FCR;	/* LCD frame control register	0x04 */
+__IO uint32_t SR;	/* LCD status register		0x08 */
+__IO uint32_t CLR;	/* LCD clear register		0x0c */
+uint32_t RESERVED;	/* Reserved			0x10 */
+__IO uint32_t RAM[16];	/* LCD display memory		0x14 */
+} LCD_TypeDef;
+
+/* Control Register 0x0 */
+#define LCD_CR_MUX_SEG		(0x1<<7)
+#define LCD_CR_BIAS_MASK	(0x3<<5)
+#define LCD_CR_BIAS_1_4		(0x0<<5)
+#define LCD_CR_BIAS_1_2		(0x1<<5)
+#define LCD_CR_BIAS_1_3		(0x2<<5)
+#define LCD_CR_DUTY_MASK	(0x7<<2)
+#define LCD_CR_DUTY_STATIC	(0x0<<2)
+#define LCD_CR_DUTY_1_2		(0x1<<2)
+#define LCD_CR_DUTY_1_3		(0x2<<2)
+#define LCD_CR_DUTY_1_4		(0x3<<2)
+#define LCD_CR_DUTY_1_8		(0x4<<2)
+#define LCD_CR_VSEL		(0x1<<1)
+#define LCD_CR_LCDEN		(0x1<<0)
+
+/* Frame Control 0x04 */
+#define LCD_FCR_CC_MASK		(0x7<<10)
+#define LCD_FCR_CC_LCD4		(0x4<<10)
+
+/* Status Register 0x8 */
+#define LCD_SR_FCRSF	(1<<5)
+#define LCD_SR_RDY	(1<<4)
+#define LCD_SR_UDR	(1<<2)
+#define LCD_SR_ENS	(1<<0)
+
+#define LCD_BASE	(APB1PERIPH_BASE + 0x2400)
+
+/* Real Time Clock */
+typedef struct {
+__IO uint32_t TR;	/* RTC time register		0x00 */
+__IO uint32_t DR;	/* RTC date register		0x04 */
+__IO uint32_t CR;	/* RTC control register		0x08 */
+__IO uint32_t ISR;	/* RTC init and status register	0x0c */
+__IO uint32_t PRER;	/* RTC prescaler register	0x10 */
+__IO uint32_t WUTR;	/* RTC wakeup timer register	0x14 */
+__IO uint32_t CALIBR;	/* RTC calibration register	0x18 */
+__IO uint32_t ALRMAR;	/* RTC alarm A register		0x1c */
+__IO uint32_t ALRMBR;	/* RTC alarm B register		0x20 */
+__IO uint32_t WPR;	/* RTC write protection reg	0x24 */
+__IO uint32_t SSR;	/* RTC sub second register	0x28 */
+__IO uint32_t SHIFTR;	/* RTC shift control register	0x2c */
+__IO uint32_t TSTR;	/* RTC time stamp time register	0x30 */
+__IO uint32_t TSDR;	/* RTC time stamp date register	0x34 */
+__IO uint32_t TSSSR;	/* RTC time-stamp sub second	0x38 */
+__IO uint32_t CALR;	/* RTC calibration register	0x3c */
+__IO uint32_t TAFCR;	/* RTC tamper/alt function	0x40 */
+__IO uint32_t ALRMASSR;	/* RTC alarm A sub second reg	0x44 */
+__IO uint32_t ALRMBSSR;	/* RTC alarm B sub second reg	0x48 */
+uint32_t RESERVED7;	/* Reserved			0x4C */
+__IO uint32_t BKP0R;	/* RTC backup register		0x50 */
+__IO uint32_t BKP1R;	/* RTC backup register		0x54 */
+__IO uint32_t BKP2R;	/* RTC backup register		0x58 */
+__IO uint32_t BKP3R;	/* RTC backup register		0x5c */
+__IO uint32_t BKP4R;	/* RTC backup register		0x60 */
+__IO uint32_t BKP5R;	/* RTC backup register		0x64 */
+__IO uint32_t BKP6R;	/* RTC backup register		0x68 */
+__IO uint32_t BKP7R;	/* RTC backup register		0x6c */
+__IO uint32_t BKP8R;	/* RTC backup register		0x70 */
+__IO uint32_t BKP9R;	/* RTC backup register		0x74 */
+__IO uint32_t BKP10R;	/* RTC backup register		0x78 */
+__IO uint32_t BKP11R;	/* RTC backup register		0x7c */
+__IO uint32_t BKP12R;	/* RTC backup register		0x80 */
+__IO uint32_t BKP13R;	/* RTC backup register		0x84 */
+__IO uint32_t BKP14R;	/* RTC backup register		0x88 */
+__IO uint32_t BKP15R;	/* RTC backup register		0x8c */
+__IO uint32_t BKP16R;	/* RTC backup register		0x90 */
+__IO uint32_t BKP17R;	/* RTC backup register		0x94 */
+__IO uint32_t BKP18R;	/* RTC backup register		0x98 */
+__IO uint32_t BKP19R;	/* RTC backup register		0x9c */
+__IO uint32_t BKP20R;	/* RTC backup register		0xa0 */
+__IO uint32_t BKP21R;	/* RTC backup register		0xa4 */
+__IO uint32_t BKP22R;	/* RTC backup register		0xa8 */
+__IO uint32_t BKP23R;	/* RTC backup register		0xac */
+__IO uint32_t BKP24R;	/* RTC backup register		0xb0 */
+__IO uint32_t BKP25R;	/* RTC backup register		0xb4 */
+__IO uint32_t BKP26R;	/* RTC backup register		0xb8 */
+__IO uint32_t BKP27R;	/* RTC backup register		0xbc */
+__IO uint32_t BKP28R;	/* RTC backup register		0xc0 */
+__IO uint32_t BKP29R;	/* RTC backup register		0xc4 */
+__IO uint32_t BKP30R;	/* RTC backup register		0xc8 */
+__IO uint32_t BKP31R;	/* RTC backup register		0xcc */
+} RTC_TypeDef;
+
+#define RTC_BASE	(APB1PERIPH_BASE + 0x2800)
+
+typedef struct {
+  __IO uint32_t CR;   /* PWR control reg		0x00 */
+  __IO uint32_t CSR;  /* PWR control status reg		0x04 */
+} PWR_TypeDef;
+
+#define PWR_CR_DBP	(1<<8)	/* Disable backup write protection */
+
+#define PWR_BASE	(APB1PERIPH_BASE + 0x7000)
 
 /* Reset and Clock Control */
 typedef struct {
@@ -21,6 +126,10 @@ __IO uint32_t APB1LPENR;	/* 0x30 */
 __IO uint32_t CSR;		/* 0x34 */
 } RCC_TypeDef;
 
+/* 0x0 Control Register */
+#define RCC_CR_MSIRDY		(0x1<<9)	/* MSI Clock Ready */
+
+/* 0x1c */
 #define AHBENR_GPIOAEN	1
 #define AHBENR_GPIOBEN	2
 #define AHBENR_GPIOCEN	4
@@ -29,6 +138,40 @@ __IO uint32_t CSR;		/* 0x34 */
 #define AHBENR_GPIOHEN	32
 #define AHBENR_GPIOFEN	64
 #define AHBENR_GPIOGEN	128
+
+/* 0x20 APB2 Clock Enable Register */
+#define RCC_APB2ENR_SYSCFGEN	(1<<0)
+
+/* 0x24 APB1 Clock Enable Register */
+#define RCC_APB1ENR_DACEN	(1<<29)	/* DAC enable */
+#define RCC_APB1ENR_PWREN	(1<<28)	/* power interface clock enable */
+#define RCC_APB1ENR_USBEN	(1<<23) /* enable USB clock */
+#define RCC_APB1ENR_I2C2EN	(1<<22) /* i2c-2 clock */
+#define RCC_APB1ENR_I2C1EN	(1<<21) /* i2c-1 clock */
+#define RCC_APB1ENR_UART5EN	(1<<20) /* uart5 clock enable */
+#define RCC_APB1ENR_UART4EN	(1<<19) /* uart4 clock enable */
+#define RCC_APB1ENR_UART3EN	(1<<18) /* uart3 clock enable */
+#define RCC_APB1ENR_UART2EN	(1<<17) /* uart2 clock enable */
+#define RCC_APB1ENR_SPI3EN	(1<<15) /* spi-3 clock enable */
+#define RCC_APB1ENR_SPI2EN	(1<<14) /* spi-2 clock enable */
+#define RCC_APB1ENR_WWDGEN	(1<<11) /* window watchdog clock enable */
+#define RCC_APB1ENR_LCDEN	(1<<9)	/* lcd clock enable */
+#define RCC_APB1ENR_TIM7EN	(1<<5)	/* timer 7 clock enable */
+#define RCC_APB1ENR_TIM6EN	(1<<4)	/* timer 6 clock enable */
+#define RCC_APB1ENR_TIM5EN	(1<<3)	/* timer 5 clock enable */
+#define RCC_APB1ENR_TIM4EN	(1<<2)	/* timer 4 clock enable */
+#define RCC_APB1ENR_TIM3EN	(1<<1)	/* timer 3 clock enable */
+#define RCC_APB1ENR_TIM2EN	(1<<0)	/* timer 2 clock enable */
+
+/* 0x34 Control-Status Register */
+#define RCC_CSR_RTCEN		(1<<22)		/* RTC enable */
+#define RCC_CSR_RTCSEL_LSE	(0x1<<16)
+#define RCC_CSR_RTCSEL_LSI	(0x2<<16)
+#define RCC_CSR_RTCSEL_HSE	(0x3<<16)
+#define RCC_CSR_LSIRDY		(0x1<<1)	/* LSI ready */
+#define RCC_CSR_LSION		(0x1<<0)	/* internal low-speed oscilator */
+
+
 
 #define RCC_BASE	(AHBPERIPH_BASE + 0x3800)
 
@@ -52,6 +195,7 @@ __IO uint32_t AFR[2];	/* 0x20 = Alternate Function (Low/High) Registers */
 
 #define GPIOA_BASE	(AHBPERIPH_BASE + 0x0000)
 #define GPIOB_BASE	(AHBPERIPH_BASE + 0x0400)
+#define GPIOC_BASE	(AHBPERIPH_BASE + 0x0800)
 
 #define MODER_INPUT	0x00
 #define MODER_OUTPUT	0x01
---
+++ stm32l/lcd_display/lcd_display.c
@@ -0,0 +1,237 @@
+/* Write something to the LCD Display */
+
+#include <stdint.h>
+
+#include "stm32l.h"
+
+#define STACK_TOP 0x20000800
+
+static void delay(int length) {
+
+	volatile int i;
+
+	for(i=0;i<length;i++) asm("nop");
+
+}
+
+
+GPIO_TypeDef *gpioa=(GPIO_TypeDef *)GPIOA_BASE;
+GPIO_TypeDef *gpiob=(GPIO_TypeDef *)GPIOB_BASE;
+GPIO_TypeDef *gpioc=(GPIO_TypeDef *)GPIOC_BASE;
+RCC_TypeDef *rcc=(RCC_TypeDef *)RCC_BASE;
+PWR_TypeDef *pwr=(PWR_TypeDef *)PWR_BASE;
+RTC_TypeDef *rtc=(RTC_TypeDef *)RTC_BASE;
+LCD_TypeDef *lcd=(LCD_TypeDef *)LCD_BASE;
+
+static void lcd_clock_init(void) {
+
+	/* enable power to clock interface */
+	rcc->APB1ENR |= RCC_APB1ENR_PWREN;
+
+	/* Disable write protection */
+	pwr->CR	|= PWR_CR_DBP;
+
+	/* Use LSI as RTC clock */
+	rcc->CSR |= RCC_CSR_RTCSEL_LSI;
+
+	/* enable RTC clock */
+	rcc->CSR |= RCC_CSR_RTCEN;
+
+	/* Disable write-protection for RTC registers */
+	rtc->WPR = 0xca;
+	rtc->WPR = 0x53;
+
+	/* Wait for MSI clock to be read */
+	while( (rcc->CR & RCC_CR_MSIRDY) == 0 ) ;
+
+	/* enable LCD clock */
+	rcc->APB1ENR |= RCC_APB1ENR_LCDEN;
+
+	/* enable SYSCFG clock */
+	rcc->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+	/* enable LSI (internal low-speed oscilator) and wait until ready */
+	rcc->CSR |= RCC_CSR_LSION;
+	while ( (rcc->CSR & RCC_CSR_LSIRDY) == 0 );
+
+	/* Select LSI as LCD Clock Source */
+	rcc->CSR &= ~RCC_CSR_RTCSEL_LSI;
+	rcc->CSR |= RCC_CSR_RTCSEL_LSI;
+
+	/* Enable RTC */
+	rcc->CSR |= RCC_CSR_RTCEN;
+}
+
+void lcd_pin_init(void) {
+
+	/* Enable GPIOA and GPIOB and GPIOC clocks*/
+	rcc->AHBENR |= (AHBENR_GPIOAEN|AHBENR_GPIOBEN|AHBENR_GPIOCEN);
+
+	/* GPIOA mode alterfunc (0b10) 15,10,9,8,3,2,1 */
+#define GPIOA_MODE_MASK	 0xc03f00fc
+#define GPIOA_MODE_ALTER 0x802a00a8
+	gpioa->MODER&=~GPIOA_MODE_MASK;
+	gpioa->MODER|=GPIOA_MODE_ALTER;
+	/* output mode?  output speed? pull-up/down? */
+
+	/* Port A, pins 15,10,9,8,3,2,1 */
+	/* Alternate function mode 0xb (LCD) */
+			/*76543210*/
+#define LCD_A_L_MASK	0x0000fff0
+#define LCD_A_L_ENABLE	0x0000bbb0
+			/*fedcba98*/
+#define	LCD_A_H_MASK	0xf0000fff
+#define LCD_A_H_ENABLE	0xb0000bbb
+
+	gpioa->AFR[0]&=~LCD_A_L_MASK;
+	gpioa->AFR[0]|=LCD_A_L_ENABLE;
+
+	gpioa->AFR[1]&=~LCD_A_H_MASK;
+	gpioa->AFR[1]|=LCD_A_H_ENABLE;
+
+
+	/* GPIOB mode alterfunc (0b10) 15,14,13,12,11,10,9,8,5,4,3 */
+#define GPIOB_MODE_MASK	 0xffff0fc0
+#define GPIOB_MODE_ALTER 0xaaaa0a80
+	gpiob->MODER&=~GPIOB_MODE_MASK;
+	gpiob->MODER|=GPIOB_MODE_ALTER;
+
+	/* Port B, pins 15,14,13,12,11,10,9,8,5,4,3 */
+	/* Alternate function mode 0xb (LCD) */
+			/*76543210*/
+#define LCD_B_L_MASK	0x00fff000
+#define LCD_B_L_ENABLE	0x00bbb000
+			/*fedcba98*/
+#define	LCD_B_H_MASK	0xffffffff
+#define LCD_B_H_ENABLE	0xbbbbbbbb
+
+	gpiob->AFR[0]&=~LCD_B_L_MASK;
+	gpiob->AFR[0]|=LCD_B_L_ENABLE;
+
+	gpiob->AFR[1]&=~LCD_B_H_MASK;
+	gpiob->AFR[1]|=LCD_B_H_ENABLE;
+
+
+	/* GPIOC mode alterfunc (0b10) 11,10,9,8,7,6,3,2,1,0 */
+#define GPIOC_MODE_MASK	 0x00fff0ff
+#define GPIOC_MODE_ALTER 0x00aaa0aa
+	gpioc->MODER&=~GPIOC_MODE_MASK;
+	gpioc->MODER|=GPIOC_MODE_ALTER;
+
+	/* Port C, pins 11,10,9,8,7,6,3,2,1,0 */
+	/* Alternate function mode 0xb (LCD) */
+			/*76543210*/
+#define LCD_C_L_MASK	0xff00ffff
+#define LCD_C_L_ENABLE	0xbb00bbbb
+			/*fedcba98*/
+#define	LCD_C_H_MASK	0x0000ffff
+#define LCD_C_H_ENABLE	0x0000bbbb
+
+	gpioc->AFR[0]&=~LCD_C_L_MASK;
+	gpioc->AFR[0]|=LCD_C_L_ENABLE;
+
+	gpioc->AFR[1]&=~LCD_C_H_MASK;
+	gpioc->AFR[1]|=LCD_C_H_ENABLE;
+
+}
+
+void lcd_config(void) {
+
+	/* Set Bias to 1/3 */
+	lcd->CR&=~LCD_CR_BIAS_MASK;
+	lcd->CR|=LCD_CR_BIAS_1_3;
+
+	/* Set Duty to 1/4 */
+	lcd->CR&=~LCD_CR_DUTY_MASK;
+	lcd->CR|=LCD_CR_DUTY_1_4;
+
+	/* contrast = mean */
+	lcd->FCR&=~LCD_FCR_CC_MASK;
+	lcd->FCR|=LCD_FCR_CC_LCD4;
+
+	/* Set enable mux segment */
+	lcd->CR|=LCD_CR_MUX_SEG;
+
+	/* select internal voltage */
+	lcd->CR&=~LCD_CR_VSEL;
+
+	/* wait until FCRSF of LCD_SR set */
+	while( !(lcd->SR & LCD_SR_FCRSF));
+
+	/* set LCDEN */
+	lcd->CR |= LCD_CR_LCDEN;
+
+	/* wait for LCDEN in LCD_SR */
+	while( !(lcd->SR & LCD_SR_ENS));
+
+	/* wait for booster for RDY in LCD_SR */
+	while( !(lcd->SR & LCD_SR_RDY));
+}
+
+
+int main(void) {
+
+	lcd_clock_init();
+
+	lcd_pin_init();
+
+	lcd_config();
+
+	/* wait until protection gone */
+	while(!(lcd->SR & LCD_SR_UDR)) ;
+
+	/* Display W */
+	lcd->RAM[0]=0x10000001;
+	lcd->RAM[2]=0x20000002;
+	lcd->RAM[4]=0x20000000;
+	lcd->RAM[6]=0x00000001;
+
+	/* request update of display */
+	lcd->SR |= LCD_SR_UDR;
+
+	/* busy wait forever */
+	for(;;) {
+
+	}
+
+}
+
+static void nmi_handler(void) {
+	for(;;);
+}
+
+static void hardfault_handler(void) {
+	for(;;);
+}
+
+extern unsigned long _etext,_data,_edata,_bss_start,_bss_end;
+
+	/* Copy DATA and BSS segments into RAM */
+void c_startup(void)	{
+
+	unsigned long *src, *dst;
+
+	/* Copy data segment */
+	/* Using linker symbols */
+	src = &_etext;
+	dst = &_data;
+	while(dst < &_edata) *(dst++) = *(src++);
+
+	/* Zero out the BSS */
+	src = &_bss_start;
+	while(src < &_bss_end) *(src++) = 0;
+
+	/* Call main() */
+	main();
+
+}
+
+
+/* Vector Table */
+unsigned int *myvectors[4]
+__attribute__ ((section("vectors"))) = {
+	(uint32_t *) STACK_TOP,		/* stack pointer      */
+	(uint32_t *) c_startup,		/* code entry point   */
+	(uint32_t *) nmi_handler,	/* NMI handler        */
+	(uint32_t *) hardfault_handler	/* hard fault handler */
+};
