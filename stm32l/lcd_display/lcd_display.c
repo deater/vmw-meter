8cd378f7585deb66ab1e8327be86324c13b82e06
Vince Weaver <vince@deater.net>
2014-02-05 15:41:37 -0500

    stm32l: work on integer printing code

--- stm32l/lcd_display/lcd_display.c
+++ stm32l/lcd_display/lcd_display.c
@@ -6,11 +6,104 @@
 #include "lcd_lib.h"
 #include "delay_lib.h"
 
-#define STACK_TOP 0x20000800
+unsigned int length=5*148*12+13;	/* us */
+
+
+int num_to_string(char *string,unsigned int value) {
+
+	int len=0,q,r,nonzero=0;
+
+	if (value==0) {
+		string[len]='0';
+		len++;
+		goto done_num_to_string;
+	}
+
+	q=value/1000000;
+	r=value%1000000;
+	/* too big */
+	if (q>10) return -1;
+
+	if (q!=0) {
+		string[len]=q+'0';
+		len++;
+		nonzero=1;
+	}
+
+	q=r;
+	r=q%100000;
+	q/=100000;
+	if (nonzero || q!=0) {
+		string[len]=q+'0';
+		len++;
+		nonzero=1;
+	}
+
+
+	q=r;
+	r=q%10000;
+	q/=10000;
+	if (nonzero || q!=0) {
+		string[len]=q+'0';
+		len++;
+		nonzero=1;
+	}
+
+
+	q=r;
+	r=q%1000;
+	q/=1000;
+	if (nonzero || q!=0) {
+		string[len]=q+'0';
+		len++;
+		nonzero=1;
+	}
+
+
+	q=r;
+	r=q%100;
+	q/=100;
+	if (nonzero || q!=0) {
+		string[len]=q+'0';
+		len++;
+		nonzero=1;
+	}
+
+
+	q=r;
+	r=q%10;
+	q/=10;
+	if (nonzero || q!=0) {
+		string[len]=q+'0';
+		len++;
+		nonzero=1;
+	}
+
+
+	q=r;
+//	r=q%1;
+	q/=1;
+	if (nonzero || q!=0) {
+		string[len]=q+'0';
+		len++;
+		nonzero=1;
+	}
+
+
+
+
+done_num_to_string:
+
+	string[len+1]=0;
+
+	return len;
+}
 
 int main(void) {
 
 	unsigned int lcd_buffer[16];
+	unsigned int value;
+	char string[7];
 
 	lcd_clock_init();
 
@@ -20,6 +113,15 @@ int main(void) {
 
 	/* busy wait forever */
 	for(;;) {
+
+	length+=1;
+
+	value=length/(148*12);
+
+	num_to_string(string,value);
+	lcd_convert(string,lcd_buffer);
+	lcd_display(lcd_buffer);
+	busy_delay(500000);
 
 	lcd_convert("VINCE ",lcd_buffer);
 	lcd_display(lcd_buffer);
@@ -87,6 +189,7 @@ void c_startup(void)	{
 
 }
 
+#define STACK_TOP 0x20000800
 
 /* Vector Table */
 unsigned int *myvectors[4]
