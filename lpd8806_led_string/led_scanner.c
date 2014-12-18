1ec482f29cdbf511acd61afecce7a0f0f73abf62
Vince Weaver <vincent.weaver@maine.edu>
2014-12-18 13:34:41 -0500

    Add simple raspberry-pi lpd8806 driver code

---
+++ lpd8806_led_string/led_scanner.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <linux/spi/spidev.h>
+
+#include "spi_lib.h"
+
+
+
+int main(int argc, char **argv) {
+
+	int spi_fd,i,j;
+	int value;
+	unsigned char zeros[128],data[128];
+	int result;
+
+	spi_fd=spi_open("/dev/spidev0.0", SPI_MODE_0, 100000, 8);
+	if (spi_fd<0) {
+		exit(-1);
+	}
+
+	/* Send a byte acting as a start bit */
+
+	for(i=0;i<128;i++) zeros[i]=0;
+	for(i=0;i<128;i++) data[i]=128;
+
+	for(i=0;i<128;i++) {
+		result=write(spi_fd,&zeros[i],1);
+		if (result<1) {
+			printf("error!\n");
+			exit(-1);
+		}
+	}
+
+#define MAX_BRIGHTNESS 64
+
+	int location=0,direction=1;
+
+	while(1) {
+
+		location+=direction;
+		if (location>31) direction=-1;
+		if (location<1) direction=1;
+
+		for(i=0;i<32;i++) {
+			if (i==location) {
+				data[(i*3)]=128+64;
+			}
+			else
+
+			if ((i==location-1) || (i==location+1)) {
+				data[(i*3)]=128+8;
+			}
+
+			else if( (i==location-2) || (i==location+2)) {
+				data[(i*3)]=128+2;
+			}
+
+
+			else {
+				data[(i*3)]=128;
+			}
+			data[(i*3)+1]=128;
+			data[(i*3)+2]=128;
+		}
+
+		for(i=0;i<128;i++) {
+			result=write(spi_fd,&data[i],1);
+			if (result<1) {
+				printf("error!\n");
+				exit(-1);
+			}
+		}
+
+	for(i=0;i<128;i++) {
+		result=write(spi_fd,&zeros[i],1);
+		if (result<1) {
+			printf("error!\n");
+			exit(-1);
+		}
+	}
+
+	}
+
+	spi_close(spi_fd);
+
+	return 0;
+}
---
+++ lpd8806_led_string/led_test.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <linux/spi/spidev.h>
+
+#include "spi_lib.h"
+
+int main(int argc, char **argv) {
+
+	int spi_fd,i,j;
+	int value;
+	unsigned char zeros[128],data[128];
+	int result;
+
+	spi_fd=spi_open("/dev/spidev0.0", SPI_MODE_0, 100000, 8);
+	if (spi_fd<0) {
+		exit(-1);
+	}
+
+	/* Send a byte acting as a start bit */
+
+	for(i=0;i<128;i++) zeros[i]=0;
+	for(i=0;i<128;i++) data[i]=128;
+
+	for(i=0;i<128;i++) {
+		result=write(spi_fd,&zeros[i],1);
+		if (result<1) {
+			printf("error!\n");
+			exit(-1);
+		}
+	}
+
+#define MAX_BRIGHTNESS 64
+
+	int brightness=0,direction=1;
+
+	while(1) {
+
+		brightness+=direction;
+		if (brightness>(MAX_BRIGHTNESS-2)) direction=-1;
+		if (brightness<1) direction=1;
+
+		data[0] = 128;
+		data[1] = 128+brightness;
+		data[2] = 128;
+	
+		data[3] = 128+((MAX_BRIGHTNESS-1)-brightness);
+		data[4] = 128;
+		data[5] = 128;
+
+
+
+		data[6] = 128;
+		data[7] = 128;
+		data[8] = 128+brightness;
+
+		for(i=0;i<128;i++) {
+			result=write(spi_fd,&data[i],1);
+			if (result<1) {
+				printf("error!\n");
+				exit(-1);
+			}
+		}
+		printf("%d\n",brightness);
+
+	for(i=0;i<128;i++) {
+		result=write(spi_fd,&zeros[i],1);
+		if (result<1) {
+			printf("error!\n");
+			exit(-1);
+		}
+	}
+
+	}
+
+	spi_close(spi_fd);
+
+	return 0;
+}
