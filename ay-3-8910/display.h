ce9f9754dc11de56ff63c1e57cc85d0593a9733d
Vince Weaver <vincent.weaver@maine.edu>
2016-09-27 13:58:13 -0400

    ay-3-8910: some minor documentation updates

--- ay-3-8910/display.h
+++ ay-3-8910/display.h
@@ -2,11 +2,11 @@
 #define DISPLAY_TEXT	0x2
 #define DISPLAY_BOTH	0x3
 
-#define MODE_TITLE	0x0
-#define MODE_VISUAL	0x1
-#define MODE_NAME	0x2
-#define MODE_TIME	0x3
-#define MODE_VOLUME	0x4
+#define MODE_TITLE	0x0	/* Prints VMW CHIPTUNE */
+#define MODE_VISUAL	0x1	/* Waterfall visualization */
+#define MODE_NAME	0x2	/* Scrolls filename */
+#define MODE_TIME	0x3	/* Shows elapsed time */
+#define MODE_VOLUME	0x4	/* Allows setting the volume */
 #define MODE_MAX	0x5
 
 #define CMD_PAUSE		0x1
