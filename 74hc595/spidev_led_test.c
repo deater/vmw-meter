65124d4ad0d7f590871cf2b5d1b53d5ccc3f142e
Vince Weaver <vincent.weaver@maine.edu>
2016-09-30 13:44:20 -0400

    74hc595: add spidev test code

---
+++ 74hc595/spidev_led_test.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+
+/* for 74hc595 */
+/* want CS active low */
+/* pi can only do MSB first(?) */
+
+int main(int argc, char **argv) {
+
+	int spi_fd;
+	struct spi_ioc_transfer spi;
+	int result = -1;
+	unsigned char data_out[3];
+	unsigned char data_in[3];
+	int new_mode;
+
+	/* Open SPI device */
+	spi_fd=open("/dev/spidev0.0", O_RDWR);
+	if (spi_fd < 0) {
+		fprintf(stderr,"Could not open SPI device "
+			"/dev/sdpidev0.0 : %s\n",
+			strerror(errno));
+		return -1;
+	}
+
+	/* Set SPI Mode_0 */
+
+        int mode=SPI_MODE_0;
+//        int mode=SPI_MODE_3;//|SPI_CS_HIGH;
+        result = ioctl(spi_fd, SPI_IOC_WR_MODE, &mode);
+        if (result < 0) {
+                fprintf(stderr,"Could not set SPI Write mode: %s\n",
+                        strerror(errno));
+                return -1;
+        }
+
+        result = ioctl(spi_fd, SPI_IOC_RD_MODE, &new_mode);
+        if (result < 0) {
+                fprintf(stderr,"Could not read SPI Write mode: %s\n",
+                        strerror(errno));
+                return -1;
+        }
+
+	printf("We are in mode %x\n",new_mode);
+
+/* Set 8 bits per word */
+
+        int bitsperword=8;
+        result = ioctl(spi_fd, SPI_IOC_WR_BITS_PER_WORD, &bitsperword);
+        if (result < 0) {
+                fprintf(stderr,"Could not set SPI WR bitsPerWord: %s\n",
+                        strerror(errno));
+                return -1;
+        }
+
+        /* Set 100 kHz max frequency */
+
+        int freq=100000;
+        result = ioctl(spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &freq);
+        if (result < 0) {
+                fprintf(stderr,"Could not set SPI WR frequency: %s\n",
+                        strerror(errno));
+                return -1;
+        }
+
+	data_out[0]=0xaa;
+	data_in[0]=0x0;
+
+	memset(&spi,0,sizeof(struct spi_ioc_transfer));
+	spi.tx_buf = (unsigned long)&data_out;
+	spi.rx_buf = (unsigned long)&data_in;
+	spi.len = 1;    /* 1 byte */
+	spi.delay_usecs = 100 ;
+	spi.speed_hz = 100000 ;
+	spi.bits_per_word = 8 ;
+	spi.cs_change = 0;
+
+	result = ioctl(spi_fd, SPI_IOC_MESSAGE(1), &spi) ;
+
+	if (result < 0){
+		fprintf(stderr,"Error sending SPI data: %s\n",strerror(errno));
+		return -1;
+	}
+
+	printf("Wrote out %x read in %x\n",data_out[0],data_in[0]);
+
+	close(spi_fd);
+
+	return 0;
+}
