87898ea1f01607bc9ad26e0d4b8b00e7b1484db3
Vince Weaver <vincent.weaver@maine.edu>
2017-01-01 16:47:03 -0500

    lpd8806: had red/green switched on two-color-scroll

--- lpd8806_led_string/two_color_scroll.c
+++ lpd8806_led_string/two_color_scroll.c
@@ -40,49 +40,49 @@ int two_color_scroll(int spi_fd, char *col1, char *col2, char *dir) {
 		for(i=0;i<32;i++) {
 			j=i+location;
 			if (j%8==0) {
-				data[(i*3)]=128+r1;	/* Bright Red */
-				data[(i*3)+1]=128+g1;
+				data[(i*3)]=128+g1;	/* Bright Red */
+				data[(i*3)+1]=128+r1;
 				data[(i*3)+2]=128+b1;
 			}
 
 			if (j%8==1) {
-				data[(i*3)]=128+r1/4;	/* Less Bright Red */
-				data[(i*3)+1]=128+g1/4;
+				data[(i*3)]=128+g1/4;	/* Less Bright Red */
+				data[(i*3)+1]=128+r1/4;
 				data[(i*3)+2]=128+b1/4;
 			}
 
 			if (j%8==2) {
-				data[(i*3)]=128+r2/4;	/* Less Bright Green */
-				data[(i*3)+1]=128+g2/4;
+				data[(i*3)]=128+g2/4;	/* Less Bright Green */
+				data[(i*3)+1]=128+r2/4;
 				data[(i*3)+2]=128+b2/4;
 			}
 
 			if (j%8==3) {
-				data[(i*3)]=128+r2;	/* Bright Green */
-				data[(i*3)+1]=128+g2;
+				data[(i*3)]=128+g2;	/* Bright Green */
+				data[(i*3)+1]=128+r2;
 				data[(i*3)+2]=128+b2;
 			}
 			if (j%8==4) {
-				data[(i*3)]=128+r2;	/* Bright Green */
-				data[(i*3)+1]=128+g2;
+				data[(i*3)]=128+g2;	/* Bright Green */
+				data[(i*3)+1]=128+r2;
 				data[(i*3)+2]=128+b2;
 			}
 
 			if (j%8==5) {
-				data[(i*3)]=128+r2/4;	/* Less Bright Green */
-				data[(i*3)+1]=128+g2/4;
+				data[(i*3)]=128+g2/4;	/* Less Bright Green */
+				data[(i*3)+1]=128+r2/4;
 				data[(i*3)+2]=128+b2/4;
 			}
 
 			if (j%8==6) {
-				data[(i*3)]=128+r1/4;	/* Less Bright Red */
-				data[(i*3)+1]=128+g1/4;
+				data[(i*3)]=128+g1/4;	/* Less Bright Red */
+				data[(i*3)+1]=128+r1/4;
 				data[(i*3)+2]=128+b1/4;
 			}
 
 			if (j%8==7) {
-				data[(i*3)]=128+r1;	/* Bright Red */
-				data[(i*3)+1]=128+g1;
+				data[(i*3)]=128+g1;	/* Bright Red */
+				data[(i*3)+1]=128+r1;
 				data[(i*3)+2]=128+b1;
 			}
 
