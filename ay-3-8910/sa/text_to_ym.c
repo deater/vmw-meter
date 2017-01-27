9d82d99406bfbdd84963aba9b94c0730b7b6476d
Vince Weaver <vincent.weaver@maine.edu>
2017-01-27 11:15:12 -0500

    ay38910: tracker generates legal file

--- ay-3-8910/sa/text_to_ym.c
+++ ay-3-8910/sa/text_to_ym.c
@@ -15,6 +15,8 @@ struct ym_header {
 	uint16_t additional_data;		// 32 -> 34
 }  __attribute__((packed)) our_header;
 
+
+
 int main(int argc, char **argv) {
 
 	char string[BUFSIZ];
@@ -24,10 +26,15 @@ int main(int argc, char **argv) {
 	int frames=0,digidrums=0,external_frequency=1000000;
 	int frequency=50;
 	int loop=0;
+	int header_length=0;
+	int i;
+	fpos_t save;
 
 	char song_name[]="Still Alive";
 	char author_name[]="Vince Weaver <vince@deater.net>";
 	char comments[]="from Portal, Words and Music by Jonathan Coulton";
+
+	unsigned char frame[16];
 
 	outfile=strdup("out.ym");
 
@@ -36,6 +43,14 @@ int main(int argc, char **argv) {
 		fprintf(stderr,"Couldn't open %s\n",outfile);
 		return -1;
 	}
+
+	/* Skip header, we'll fill in later */
+	header_length=sizeof(struct ym_header)+
+		strlen(song_name)+1+
+		strlen(author_name)+1+
+		strlen(comments)+1;
+
+	fseek(fff, header_length, SEEK_SET);
 
 	while(1) {
 		result=fgets(string,BUFSIZ,stdin);
@@ -46,24 +61,33 @@ int main(int argc, char **argv) {
 		if (string[0]=='-') continue;
 
 		printf("%s",string);
+		for(i=0;i<16;i++) {
+			fprintf(fff,"%c",frame[i]);
+		}
+		frames++;
+
 	}
+
+	fgetpos(fff,&save);
 
 	rewind(fff);
 
-	strcpy(our_header.id,"YM5!");
-	strcpy(our_header.check,"LeOnArD!");
+	strncpy(our_header.id,"YM5!",4);
+	strncpy(our_header.check,"LeOnArD!",8);
 	our_header.vbl=htonl(frames);
 	our_header.digidrum=htonl(digidrums);
 	our_header.external_frequency=htonl(external_frequency);
-	our_header.player_frequency=htonl(frequency);
+	our_header.player_frequency=htons(frequency);
 	our_header.loop=htonl(loop);
-	our_header.additional_data=htonl(0);
+	our_header.additional_data=htons(0);
 
 	fwrite(&our_header,sizeof(struct ym_header),1,fff);
 
 	fprintf(fff,"%s%c",song_name,0);
 	fprintf(fff,"%s%c",author_name,0);
 	fprintf(fff,"%s%c",comments,0);
+
+	fsetpos(fff,&save);
 
 	fprintf(fff,"End!");
 
