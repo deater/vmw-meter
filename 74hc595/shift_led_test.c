511860f10455c882debe0ab0029da72a71b11b6b
Vince Weaver <vincent.weaver@maine.edu>
2017-03-20 16:48:47 -0400

    74hc595: make it work with SPI pins
    works fine, why won't SPI drive it :(

--- 74hc595/shift_led_test.c
+++ 74hc595/shift_led_test.c
@@ -9,7 +9,20 @@
 
 /* UGH!  Upstream kernels start GPIO at 970 on the Pi for some reason */
 /* I'm developing this on upstream 4.8-rc3 for some reason */
-#define KERNEL 970
+//#define KERNEL 970
+
+#define KERNEL 0
+
+#if 1
+#define GPIO_MOSI	10
+#define GPIO_CLK	11
+#define GPIO_CE0	8
+#else
+#define GPIO_MOSI	17
+#define GPIO_CLK	27
+#define GPIO_CE0	22
+#endif
+
 
 #include <stdio.h>
 #include <unistd.h>
@@ -23,40 +36,40 @@ int main(int argc, char **argv) {
 	printf("74HC595 LED Test test\n");
 
 	/* Enable GPIO17 */
-	gpio_enable(17+KERNEL);
-	gpio_set_write(17+KERNEL);
+	gpio_enable(GPIO_MOSI+KERNEL);
+	gpio_set_write(GPIO_MOSI+KERNEL);
 
 	/* Enable GPIO27 */
-	gpio_enable(27+KERNEL);
-	gpio_set_write(27+KERNEL);
+	gpio_enable(GPIO_CLK+KERNEL);
+	gpio_set_write(GPIO_CLK+KERNEL);
 
 	/* Enable GPIO22 */
-	gpio_enable(22+KERNEL);
-	gpio_set_write(22+KERNEL);
+	gpio_enable(GPIO_CE0+KERNEL);
+	gpio_set_write(GPIO_CE0+KERNEL);
 
 	while(1) {
 
 		for(i=0;i<8;i++) {
 
 			/* Set clock low */
-			gpio_write(27+KERNEL,0);
+			gpio_write(GPIO_CLK+KERNEL,0);
 
 			/* Set bit to output */
 			if (odd) {
-				gpio_write(17+KERNEL,i&1);
+				gpio_write(GPIO_MOSI+KERNEL,i&1);
 			}
 			else {
-				gpio_write(17+KERNEL,!(i&1));
+				gpio_write(GPIO_MOSI+KERNEL,!(i&1));
 			}
 
 			/* Set clock high */
-			gpio_write(27+KERNEL,1);
+			gpio_write(GPIO_CLK+KERNEL,1);
 		}
 
 		/* Write to output buffer */
 
-		gpio_write(22+KERNEL,1);
-		gpio_write(22+KERNEL,0);
+		gpio_write(GPIO_CE0+KERNEL,1);
+		gpio_write(GPIO_CE0+KERNEL,0);
 
 		sleep(1);
 		odd=!odd;
