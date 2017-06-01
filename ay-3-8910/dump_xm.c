9c46c81db9817f235a428148cc238e7620c00223
Vince Weaver <vince@deater.net>
2017-05-31 20:32:49 -0400

    ay-3-8910: dump_xm: make it into a library

--- ay-3-8910/dump_xm.c
+++ ay-3-8910/dump_xm.c
@@ -33,18 +33,23 @@ void note_to_string(int note) {
 }
 
 
+struct pattern_element_struct {
+	unsigned char note,instrument,volume,effect,param;
+};
 
-int main(int argc, char **argv) {
+struct pattern_struct {
+	int num_rows;
+	int packing_type;
+	struct pattern_element_struct p[256][4];
+};
 
-	char *filename;
-	int fd,result;
-	unsigned char first_header[FIRST_HEADER_LENGTH];
-	char module_name[20];
-	char tracker_name[20];
+// FIXME: bigger channels, up to 16 or more?
+// Or calloc it based on num_channels?
+
+struct xm_info_struct {
+	char module_name[21];
+	char tracker_name[21];
 	int version_high,version_low;
-	int header_length;
-
-	unsigned char *header=NULL;
 	int song_length;
 	int restart_position;
 	int number_of_channels;
@@ -53,24 +58,105 @@ int main(int argc, char **argv) {
 	int flags;
 	int default_tempo;
 	int default_bpm;
+	unsigned char pattern_order[256];
+	struct pattern_struct pattern[256];
+};
+
+int dump_xm_file(struct xm_info_struct *xm) {
+
+	int i,j,c;
+	int pattern_break;
+
+	printf("NAME: %s\n",xm->module_name);
+	printf("TRACKER: %s\n",xm->tracker_name);
+	printf("VERSION %d.%d\n",xm->version_high,xm->version_low);
+	printf("SONG LEN: %d patterns\n",xm->song_length);
+	printf("LOOP: %d\n",xm->restart_position);
+	printf("CHANNELS: %d\n",xm->number_of_channels);
+	printf("PATTERNS: %d\n",xm->number_of_patterns);
+	printf("INSTRUMENTS: %d\n",xm->number_of_instruments);
+	printf("FLAGS: %x (%s)\n",xm->flags,
+			xm->flags==0?"Amiga Freq":"Linear Freq");
+	printf("TEMPO: %d\n",xm->default_tempo);
+	printf("BPM: %d\n",xm->default_bpm);
+
+	printf("Pattern order:\n");
+	for(i=0;i < xm->song_length;i++) {
+		printf("%x ",xm->pattern_order[i]);
+	}
+	printf("\n");
+
+	for(i=0;i < xm->number_of_patterns;i++) {
+
+		printf("Pattern %x, ",i);
+		printf("Num_Rows: %d\n",xm->pattern[i].num_rows);
+
+		pattern_break=0;
+
+		for(j=0; j < xm->pattern[i].num_rows;j++) {
+			printf("%02X ",j);
+			for(c=0;c<4;c++) {
+
+				note_to_string(xm->pattern[i].p[j][c].note);
+
+				printf(".");
+
+				if (xm->pattern[i].p[j][c].instrument) {
+					printf("%X",xm->pattern[i].p[j][c].instrument);
+				}
+				else {
+					printf(".");
+				}
+
+				if (xm->pattern[i].p[j][c].volume) {
+					printf("%02X",xm->pattern[i].p[j][c].volume-0x10);
+				}
+				else {
+					printf("..");
+				}
+
+				if ((xm->pattern[i].p[j][c].effect) ||
+					(xm->pattern[i].p[j][c].param)) {
+						printf("%X%02X",
+							xm->pattern[i].p[j][c].effect,
+							xm->pattern[i].p[j][c].param);
+				}
+				else {
+					printf("...");
+				}
+
+				printf(" ");
+
+				if (xm->pattern[i].p[j][c].effect==0xd) pattern_break=1;
+			}
+			printf("\n");
+			if (pattern_break) break;
+		}
+	}
+
+	return 0;
+}
+
+int load_xm_file(char *filename, struct xm_info_struct *xm) {
+
+	int fd,result;
+	unsigned char first_header[FIRST_HEADER_LENGTH];
+	int header_length;
+
+	unsigned char *header=NULL;
 	int i,j;
 
 	unsigned char pattern_header[2048];
 	unsigned char packed_pattern[2048];
 
 	int pattern_header_length;
-	int packing_type;
-	int num_rows;
 	int packed_size;
-
-
-
-	filename=strdup("stillalive.xm");
+	int line;
 
 	fd=open(filename,O_RDONLY);
 	if (fd<0) {
 		fprintf(stderr,"Error opening %s!\n",filename);
-		exit(-1);
+		return -1;
 	}
 
 	/*********************/
@@ -80,34 +166,37 @@ int main(int argc, char **argv) {
 	result=read(fd,first_header,FIRST_HEADER_LENGTH);
 	if (result!=FIRST_HEADER_LENGTH) {
 		fprintf(stderr,"Error reading first header!\n");
-		exit(-1);
+		return -1;
 	}
 
 	/* validate */
 	if (memcmp(first_header,"Extended Module: ",17)) {
 		fprintf(stderr,"ID text does not match!\n");
-		exit(-1);
+		return -1;
 	}
-	memcpy(module_name,first_header+17,20);
-	printf("NAME: %s\n",module_name);
 
+	/* module name */
+	memcpy(xm->module_name,first_header+17,20);
+	xm->module_name[20]=0;
+
+	/* char */
 	if (first_header[37]!=0x1a) {
 		fprintf(stderr,"Random char doesn't match!\n");
-		exit(-1);
+		return -1;
 	}
 
-	memcpy(tracker_name,first_header+38,20);
-	printf("TRACKER: %s\n",tracker_name);
+	/* tracker name */
+	memcpy(xm->tracker_name,first_header+38,20);
+	xm->tracker_name[20]=0;
 
 	/* Little Endian??? */
-	version_low=first_header[58];
-	version_high=first_header[59];
-	printf("VERSION %d.%d\n",version_high,version_low);
+	xm->version_low=first_header[58];
+	xm->version_high=first_header[59];
 
 	/* Little endian???? */
 	header_length=(first_header[63]<<24)+(first_header[62]<<16)+
 			(first_header[61]<<8)+first_header[60];
-	printf("HEADER LEN: %d\n",header_length);
+	//printf("HEADER LEN: %d\n",header_length);
 
 	/****************************/
 	/* Load actual header       */
@@ -116,83 +205,78 @@ int main(int argc, char **argv) {
 	header=calloc(header_length,sizeof(char));
 	if (header==NULL) {
 		fprintf(stderr,"Error allocating header!\n");
-		exit(-1);
+		return -1;
 	}
 
 	result=read(fd,header+4,header_length-4);
 	if (result!=(header_length-4)) {
 		fprintf(stderr,"Error reading header!\n");
-		exit(-1);
+		return -1;
 	}
 
-	song_length=header[4]+(header[5]<<8);
-	printf("SONG LEN: %d patterns\n",song_length);
+	/* Song Length */
+	xm->song_length=header[4]+(header[5]<<8);
 
-	restart_position=header[6]+(header[7]<<8);
-	printf("LOOP: %d\n",restart_position);
+	/* Restart Position */
+	xm->restart_position=header[6]+(header[7]<<8);
 
-	number_of_channels=header[8]+(header[9]<<8);
-	printf("CHANNELS: %d\n",number_of_channels);
+	/* Channels */
+	xm->number_of_channels=header[8]+(header[9]<<8);
 
-	number_of_patterns=header[10]+(header[11]<<8);
-	printf("PATTERNS: %d\n",number_of_patterns);
+	/* Patterns */
+	xm->number_of_patterns=header[10]+(header[11]<<8);
 
-	number_of_instruments=header[12]+(header[13]<<8);
-	printf("INSTRUMENTS: %d\n",number_of_instruments);
+	/* Instruments */
+	xm->number_of_instruments=header[12]+(header[13]<<8);
 
-	flags=header[14]+(header[15]<<8);
-	printf("FLAGS: %x (%s)\n",flags,flags==0?"Amiga Freq":"Linear Freq");
+	/* Flags */
+	xm->flags=header[14]+(header[15]<<8);
 
-	default_tempo=header[16]+(header[17]<<8);
-	printf("TEMPO: %d\n",default_tempo);
+	/* Tempo */
+	xm->default_tempo=header[16]+(header[17]<<8);
 
-	default_bpm=header[18]+(header[19]<<8);
-	printf("BPM: %d\n",default_bpm);
+	/* BPM */
+	xm->default_bpm=header[18]+(header[19]<<8);
 
-	printf("Pattern order:\n");
-	for(i=0;i<song_length;i++) {
-		printf("%x ",header[20+i]);
+	/* Pattern Order */
+	for(i=0;i<xm->song_length;i++) {
+		xm->pattern_order[i]=header[20+i];
 	}
-	printf("\n");
 
 	/* Load patterns */
 
-	int line;
-
-	int note,instrument,volume,effect,param;
 
 
-	for(i=0;i<number_of_patterns;i++) {
-		printf("Pattern %x\n",i);
+	for(i=0;i < xm->number_of_patterns;i++) {
 
 		result=read(fd,pattern_header,9);
 		if (result!=9) {
 			fprintf(stderr,"Error reading pattern header %d!\n",i);
-			exit(-1);
+			return -1;
 		}
 
 		pattern_header_length=pattern_header[0]+
 					(pattern_header[1]<<8)+
 					(pattern_header[2]<<16)+
 					(pattern_header[3]<<24);
-		printf("\tPattern Header Length: %d\n",pattern_header_length);
+		//printf("\tPattern Header Length: %d\n",pattern_header_length);
 
 		if (pattern_header_length!=9) {
 			fprintf(stderr,"Error invalid pattern header size %d!\n",i);
-			exit(-1);
+			return -1;
 		}
 
-		packing_type=pattern_header[4];
-		if (packing_type!=0) {
+		xm->pattern[i].packing_type=pattern_header[4];
+		if (xm->pattern[i].packing_type!=0) {
 			fprintf(stderr,"Error! packing type not zero!\n");
-			exit(-1);
+			return -1;
 		}
 
-		num_rows=pattern_header[5]+(pattern_header[6]<<8);
-		printf("\tNum_Rows: %d\n",num_rows);
+		xm->pattern[i].num_rows=
+			pattern_header[5]+(pattern_header[6]<<8);
 
 		packed_size=pattern_header[7]+(pattern_header[8]<<8);
-		printf("\tPacked Pattern Size: %d\n",packed_size);
+		//printf("\tPacked Pattern Size: %d\n",packed_size);
 
 		result=read(fd,packed_pattern,packed_size);
 		if (result!=packed_size) {
@@ -208,59 +292,52 @@ int main(int argc, char **argv) {
 
 		while(1) {
 			if (channel==0) {
-				printf("%2X ",line);
+//				printf("%2X ",line);
 				pattern_break=0;
 			}
 
-			note=0;
-			instrument=0;
-			volume=0;
-			effect=0;
-			param=0;
+			xm->pattern[i].p[line][channel].note=0;
+			xm->pattern[i].p[line][channel].instrument=0;
+			xm->pattern[i].p[line][channel].volume=0;
+			xm->pattern[i].p[line][channel].effect=0;
+			xm->pattern[i].p[line][channel].param=0;
 
 			ch=packed_pattern[j];
 
 			if (ch&0x80) {
-				//printf("!%x!",ch);
 				j++;
 				if (ch&0x1) {
-					note=packed_pattern[j];
+					xm->pattern[i].p[line][channel].note=
+						packed_pattern[j];
 					j++;
 				}
 				if (ch&0x2) {
-					instrument=packed_pattern[j];
+					xm->pattern[i].p[line][channel].instrument=
+						packed_pattern[j];
 					j++;
 				}
 				if (ch&0x4) {
-					volume=packed_pattern[j];
+					xm->pattern[i].p[line][channel].volume=
+						packed_pattern[j];
 					j++;
 				}
 				if (ch&0x8) {
-					effect=packed_pattern[j];
+					xm->pattern[i].p[line][channel].effect=
+						packed_pattern[j];
 					j++;
 				}
 				if (ch&0x10) {
-					param=packed_pattern[j];
+					xm->pattern[i].p[line][channel].param=
+						packed_pattern[j];
 					j++;
 				}
 
-				note_to_string(note);
-				printf(".");
-				if (instrument) printf("%X",instrument);
-				else printf(".");
-				if (volume) printf("%02X",volume-0x10);
-				else printf("..");
-				if ((effect) || (param)) printf("%X",effect);
-				else printf(".");
-				if ((effect)||(param)) printf("%02X",param);
-				else printf("..");
-				printf(" ");
-
-				if (effect==0xd) pattern_break=1;
+				if (xm->pattern[i].p[line][channel].effect==0xd)
+					pattern_break=1;
 
 				channel++;
-				if (channel>=number_of_channels) {
-					printf("\n");
+				if (channel>=xm->number_of_channels) {
+//					printf("\n");
 					channel=0;
 					line++;
 					if (pattern_break) break;
@@ -273,7 +350,7 @@ int main(int argc, char **argv) {
 
 			if (j>=packed_size) break;
 		}
-		printf("\n");
+//		printf("\n");
 
 	}
 
@@ -283,4 +360,30 @@ int main(int argc, char **argv) {
 
 	return 0;
 
+}
+
+
+int main(int argc, char **argv) {
+
+	struct xm_info_struct xm;
+	int result;
+	char *filename;
+
+	if (argc>1) {
+		filename=argv[1];
+	}
+	else {
+		filename=strdup("stillalive.xm");
+	}
+
+	result=load_xm_file(filename,&xm);
+
+	if (result<0) {
+
+	}
+	else {
+		dump_xm_file(&xm);
+	}
+
+	return 0;
 }
