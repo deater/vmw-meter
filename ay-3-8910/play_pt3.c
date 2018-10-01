07ab427afff249b5bb3b02e7400e58e78ee9a026
Vince Weaver <vincent.weaver@maine.edu>
2018-10-01 14:34:37 -0400

    play_pt3: start translating from z80

--- ay-3-8910/play_pt3.c
+++ ay-3-8910/play_pt3.c
@@ -14,25 +14,21 @@
 
 #include <bcm2835.h>
 
-#include "stats.h"
+//#include "stats.h"
 #include "ay-3-8910.h"
-#include "display.h"
-#include "ym_lib.h"
+//#include "display.h"
+//#include "ym_lib.h"
 #include "max98306.h"
-#include "visualizations.h"
+//#include "visualizations.h"
 
 #include "version.h"
 
 //#define AY38910_CLOCK	1000000	/* 1MHz on our board */
 
 static int play_music=1;
-static int mute_channel=0;
 static int dump_info=0;
 static int shift_size=16;
 static int music_repeat=0;
-static int music_paused=0;
-static int music_loop=0;
-static int volume=1;
 
 static void quiet_and_exit(int sig) {
 
@@ -65,194 +61,14 @@ static void print_help(int just_version, char *exec_name) {
 	exit(0);
 }
 
-#define TEXT_MODE_BANNER	0
-#define TEXT_MODE_FILENAME	1
-#define TEXT_MODE_SONGNAME	2
-#define TEXT_MODE_AUTHOR	3
-#define TEXT_MODE_TIMER		4
-#define TEXT_MODE_MENU		5
-
-
-/* TIMING */
-/* for 50Hz have a 20ms deadline */
-/* Currently (with GPIO music and Linux i2c):			   */
-/*   ym_play_frame:        14ms (need to move to SPI!)             */
-/*   display_update:        1-3ms (depends on if all are updated)  */
-/*   display_keypad_read: 0.5ms                                    */
-/*   display_string:        5ms (slow!  3 i2c addresses            */
-/*				5.02ms (Linux) 4.9ms (libbcm)	   */
-
-static int play_song(char *filename) {
-
-	int length_seconds;
-	double s,n,hz,diff;
-	int result;
-	int frame_num=0,frames_elapsed=0;
-
-	struct timeval start,next;
-
-	struct ym_song_t ym_song;
-	struct display_stats ds;
-
-#define TIMING_DEBUG	0
-
-#if TIMING_DEBUG==1
-	struct timeval before,after;
-	double b,a;
-	FILE *debug_file;
-	debug_file=fopen("timing.debug","w");
-#endif
-
-	printf("\nPlaying song %s\n",filename);
-
-	result=load_ym_song(filename,&ym_song);
-	if (result<0) {
-		return -1;
-	}
-
-	gettimeofday(&start,NULL);
-
-	/**********************/
-	/* Print song summary */
-	/**********************/
-
-	printf("\tYM%d",ym_song.type);
-	printf("\tSong attributes (%d) : ",ym_song.attributes);
-	printf("Interleaved=%s\n",ym_song.interleaved?"yes":"no");
-	if (ym_song.num_digidrum>0) {
-		printf("Num digidrum samples: %d\n",ym_song.num_digidrum);
-	}
-	printf("\tFrames: %d, ",ym_song.num_frames);
-	printf("Chip clock: %d Hz, ",ym_song.master_clock);
-	printf("Frame rate: %d Hz, ",ym_song.frame_rate);
-	if (ym_song.frame_rate!=50) {
-		fprintf(stderr,"FIX ME framerate %d\n",ym_song.frame_rate);
-		exit(1);
-	}
-	length_seconds=ym_song.num_frames/ym_song.frame_rate;
-	printf("Length=%d:%02d\n",length_seconds/60,length_seconds%60);
-	printf("\tLoop frame: %d, ",ym_song.loop_frame);
-	printf("Extra data size: %d\n",ym_song.extra_data);
-	printf("\tSong name: %s\n",ym_song.song_name);
-	printf("\tAuthor name: %s\n",ym_song.author);
-	printf("\tComment: %s\n",ym_song.comment);
-
-	/******************/
-	/* Play the song! */
-	/******************/
-
-	frame_num=0;
-	while(1) {
-
-		if (!music_paused) {
-
-#if TIMING_DEBUG==1
-		gettimeofday(&before,NULL);
-#endif
-
-			ym_play_frame(&ym_song,frame_num,shift_size,
-					&ds,0,
-					play_music,
-					mute_channel);
-
-
-#if TIMING_DEBUG==1
-		gettimeofday(&after,NULL);
-		b=before.tv_sec+(before.tv_usec/1000000.0);
-		a=after.tv_sec+(after.tv_usec/1000000.0);
-		fprintf(debug_file,"T: %lf\n",(a-b));
-#endif
-
-		}
-
-		/* increment frame */
-		if (!music_paused) frame_num++;
-
-		/* Check to see if done with file */
-		if (frame_num>=ym_song.num_frames) {
-			if (music_loop) {
-				frame_num=ym_song.loop_frame;
-			}
-			else {
-				break;
-			}
-		}
-
-		frames_elapsed++;
-
-		/* Calculate time it took to play/visualize */
-		gettimeofday(&next,NULL);
-		s=start.tv_sec+(start.tv_usec/1000000.0);
-		n=next.tv_sec+(next.tv_usec/1000000.0);
-		diff=(n-s)*1000000.0;
-
-		/* Delay until time for next update (often 50Hz) */
-		if (play_music) {
-			if (diff>0) bcm2835_delayMicroseconds(20000-diff);
-			/* often 50Hz = 20000 */
-			/* TODO: calculate correctly */
-#if TIMING_DEBUG==1
-			fprintf(debug_file,"D: %lf\n",(20000-diff));
-#endif
-		}
-
-		/* Calculate time it actually took, and print		*/
-		/* so we can see if things are going horribly wrong	*/
-		gettimeofday(&next,NULL);
-		n=next.tv_sec+(next.tv_usec/1000000.0);
-
-		if (frame_num%100==0) {
-			hz=1/(n-s);
-			printf("Done frame %d/%d, %.1lfHz\n",
-				frame_num,ym_song.num_frames,hz);
-		}
-		start.tv_sec=next.tv_sec;
-		start.tv_usec=next.tv_usec;
-
-
-
-	}
-
-	if (frame_num>ym_song.num_frames) {
-		printf("Fast-forwarded, skipping end check\n");
-	}
-	else {
-		int file_offset;
-
-		file_offset=(frame_num)*ym_song.frame_size;
-
-		if (ym_song.type>3) {
-			/* Read the tail of the file and ensure */
-			/* it has the proper trailer */
-			if (memcmp(&ym_song.frame_data[file_offset],"End!",4)) {
-				fprintf(stderr,"ERROR! Bad ending! %x\n",
-					ym_song.frame_data[file_offset]);
-				return -1;
-			}
-		}
-
-	}
-
-	/* Free the ym file */
-	free(ym_song.file_data);
-
-#if TIMING_DEBUG==1
-	fclose(debug_file);
-#endif
-
-//	if (dump_info) {
-//		printf("Max a=%.2lf b=%.2lf c=%.2lf\n",max_a,max_b,max_c);
-//	}
-
-	return 0;
-}
+extern int play_pt3(char *filename);
 
 
 int main(int argc, char **argv) {
 
 	char filename[BUFSIZ]="ea.pt3";
 	int result;
-	int c,first_song=0,next_song;
+	int c,first_song=0,next_song=0;
 
 	/* Setup control-C handler to quiet the music	*/
 	/* otherwise if you force quit it keeps playing	*/
@@ -314,19 +130,7 @@ int main(int argc, char **argv) {
 		}
 
 		/* Play the song */
-		result=play_song(filename);
-		if (result==CMD_EXIT_PROGRAM) {
-			break;
-		}
-
-		if (result==CMD_BACK) {
-			next_song-=2;
-			if (next_song<0) next_song=0;
-		}
-		if (result==CMD_NEXT) {
-			/* already point to next song */
-		}
-
+		result=play_pt3(filename);
 
 		/* Quiet down the chips */
 		if (play_music) {
