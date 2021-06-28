e1a8a549992fa5b640cd47fe06da4e2111680fd1
Vince Weaver <vincent.weaver@maine.edu>
2021-06-28 15:04:24 -0400

    pi-pico: read button on GPIO

--- pi-pico/include/gpio.h
+++ pi-pico/include/gpio.h
@@ -1,3 +1,11 @@
+/* see ./host/hardware_gpio/include/hardware/gpio.h */
+
+#define GPIO_OUT 1
+#define GPIO_IN	0
+
+#define NUM_BANK0_GPIOS	30
+
 void gpio_init(uint32_t which);
 void gpio_set_dir(uint32_t which, uint32_t dir);
 void gpio_put(uint32_t which, uint32_t value);
+uint32_t gpio_get(uint32_t which);
--- pi-pico/include/pi-pico.h
+++ pi-pico/include/pi-pico.h
@@ -1,7 +1,3 @@
 /* see ./boards/include/boards/pico.h */
 
 #define PICO_DEFAULT_LED_PIN 25
-
-/* see ./host/hardware_gpio/include/hardware/gpio.h */
-
-#define GPIO_OUT 1
