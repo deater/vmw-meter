5da7687ba4a296f39f945295d1b75fd77503a453
Vince Weaver <vincent.weaver@maine.edu>
2022-12-05 15:16:45 -0500

    mpl115a2: separate out header file

--- mpl115a2-i2c/mpl115a2.c
+++ mpl115a2-i2c/mpl115a2.c
@@ -17,6 +17,8 @@
 
 #include "i2c_lib.h"
 
+#include "mpl115a2.h"
+
 static double c_to_f(double c) {
 
 	return ((9.0/5.0)*c)+32;
@@ -54,23 +56,6 @@ int main(int argc, char **argv) {
 		fprintf(stderr,"Error opening device!\n");
 		return -1;
 	}
-
-#define MPL115A2_ADDR	0x60
-
-
-#define MPL115A2_REGISTER_PRESSURE_MSB		0x00 // 10-bit pressure MSB
-#define MPL115A2_REGISTER_PRESSURE_LSB		0x01 // 10-bit Pressure LSB
-#define MPL115A2_REGISTER_TEMP_MSB		0x02 // 10-bit Temperature MSB
-#define MPL115A2_REGISTER_TEMP_LSB		0x03 // 10-bit Temperature LSB
-#define MPL115A2_REGISTER_A0_COEFF_MSB		0x04 // a0 coefficient MSB
-#define MPL115A2_REGISTER_A0_COEFF_LSB		0x05 // a0 coefficient LSB
-#define MPL115A2_REGISTER_B1_COEFF_MSB		0x06 // b1 coefficient MSB
-#define MPL115A2_REGISTER_B1_COEFF_LSB		0x07 // b1 coefficient LSB
-#define MPL115A2_REGISTER_B2_COEFF_MSB		0x08 // b2 coefficient MSB
-#define MPL115A2_REGISTER_B2_COEFF_LSB		0x09 // b2 coefficient LSB
-#define MPL115A2_REGISTER_C12_COEFF_MSB		0x0A // c12 coefficient MSB
-#define MPL115A2_REGISTER_C12_COEFF_LSB		0x0B // c12 coefficient LSB
-#define MPL115A2_REGISTER_STARTCONVERSION 	0x12 // start conversion
 
 	/* Init MPL115A2 */
 	if (ioctl(i2c_fd, I2C_SLAVE, MPL115A2_ADDR) < 0) {
---
+++ mpl115a2-i2c/mpl115a2.h
@@ -0,0 +1,16 @@
+#define MPL115A2_ADDR	0x60
+
+#define MPL115A2_REGISTER_PRESSURE_MSB		0x00 // 10-bit pressure MSB
+#define MPL115A2_REGISTER_PRESSURE_LSB		0x01 // 10-bit Pressure LSB
+#define MPL115A2_REGISTER_TEMP_MSB		0x02 // 10-bit Temperature MSB
+#define MPL115A2_REGISTER_TEMP_LSB		0x03 // 10-bit Temperature LSB
+#define MPL115A2_REGISTER_A0_COEFF_MSB		0x04 // a0 coefficient MSB
+#define MPL115A2_REGISTER_A0_COEFF_LSB		0x05 // a0 coefficient LSB
+#define MPL115A2_REGISTER_B1_COEFF_MSB		0x06 // b1 coefficient MSB
+#define MPL115A2_REGISTER_B1_COEFF_LSB		0x07 // b1 coefficient LSB
+#define MPL115A2_REGISTER_B2_COEFF_MSB		0x08 // b2 coefficient MSB
+#define MPL115A2_REGISTER_B2_COEFF_LSB		0x09 // b2 coefficient LSB
+#define MPL115A2_REGISTER_C12_COEFF_MSB		0x0A // c12 coefficient MSB
+#define MPL115A2_REGISTER_C12_COEFF_LSB		0x0B // c12 coefficient LSB
+#define MPL115A2_REGISTER_STARTCONVERSION 	0x12 // start conversion
+
